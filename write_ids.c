b756948465c8b634559cd32fdf75f781148fedf3
Jacob Parisi <jmp7838@psu.edu>
2024-09-16 01:10:39 -0400

    Added git username and psu 9-digit id

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = lamboracer1;
+    char STUDENT_ID[] = 937699353;
 
     return 0 ;
 }
